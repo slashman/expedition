90dd93a751dedb0c98ec3bba176bf9ed65637522
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2009-03-22 07:01:18 +0000

    Implementing Serializable and allowing unknown longitude

--- src/net/slashie/expedition/level/ExpeditionLevelHelper.java
+++ src/net/slashie/expedition/level/ExpeditionLevelHelper.java
@@ -1,9 +1,11 @@
 package net.slashie.expedition.level;
 
+import java.io.Serializable;
+
 import net.slashie.expedition.world.ExpeditionLevel;
 import net.slashie.util.Pair;
 
-public class ExpeditionLevelHelper {
+public class ExpeditionLevelHelper implements Serializable{
 	private ExpeditionLevel expeditionLevel;
 	public ExpeditionLevelHelper(ExpeditionLevel expeditionLevel) {
 		super();
@@ -30,7 +32,8 @@ public class ExpeditionLevelHelper {
 	public Pair<String,String> getLocationDescription(){
 		Pair<Integer, Integer> location = expeditionLevel.getLocation();
 		handyReusableObject.setA(Math.abs(location.getA()) + (location.getA() > 0?"N":"S"));
-		handyReusableObject.setB(Math.abs(location.getB()) + (location.getB() > 0?"W":"E"));
+		//handyReusableObject.setB(Math.abs(location.getB()) + (location.getB() > 0?"E":"W"));
+		handyReusableObject.setB("???");
 		
 		return handyReusableObject;
 	}
