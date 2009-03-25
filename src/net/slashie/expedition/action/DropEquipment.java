fad1aaa4b2c3e46409cd7ec569f570d8010034a1
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2009-03-25 03:34:20 +0000

    Add droping items into the sea

--- src/net/slashie/expedition/action/DropEquipment.java
+++ src/net/slashie/expedition/action/DropEquipment.java
@@ -1,6 +1,7 @@
 package net.slashie.expedition.action;
 
 import net.slashie.expedition.domain.GoodsCache;
+import net.slashie.expedition.domain.SeaPseudoCache;
 import net.slashie.expedition.game.ExpeditionGame;
 import net.slashie.expedition.ui.ExpeditionUserInterface;
 import net.slashie.expedition.world.ExpeditionMacroLevel;
@@ -11,27 +12,33 @@ import net.slashie.serf.game.Player;
 import net.slashie.serf.ui.UserInterface;
 import net.slashie.utils.Position;
 
-public class BuildCache extends Action{
+public class DropEquipment extends Action{
 
 	@Override
 	public void execute() {
+		if (((OverworldExpeditionCell) performer.getLevel().getMapCell(performer.getPosition())).isLand()){
+			GoodsCache cache = new GoodsCache((ExpeditionGame)((Player)performer).getGame());
+			cache.setPosition(new Position(performer.getPosition()));
+			((ExpeditionUserInterface)UserInterface.getUI()).transferFromExpedition(cache);
+			if (cache.getItems().size() > 0)
+				performer.getLevel().addFeature(cache);
+		} else {
+			//Drop things into the big sea
+			GoodsCache cache = new SeaPseudoCache((ExpeditionGame)((Player)performer).getGame());
+			((ExpeditionUserInterface)UserInterface.getUI()).transferFromExpedition(cache);
+		}
+
 		
-		GoodsCache cache = new GoodsCache((ExpeditionGame)((Player)performer).getGame());
-		cache.setPosition(new Position(performer.getPosition()));
-		((ExpeditionUserInterface)UserInterface.getUI()).transferFromExpedition(cache);
-		performer.getLevel().addFeature(cache);
 	}
 
 	@Override
 	public String getID() {
-		return "BuildCache";
+		return "DropEquipment";
 	}
 	
 	@Override
 	public boolean canPerform(Actor a) {
 		if (!(a.getLevel() instanceof ExpeditionMacroLevel))
-			return false;
-		if (!((OverworldExpeditionCell) a.getLevel().getMapCell(a.getPosition())).isLand())
 			return false;
 		return true;
 	}
@@ -39,9 +46,7 @@ public class BuildCache extends Action{
 	@Override
 	public String getInvalidationMessage() {
 		if (!(performer.getLevel() instanceof ExpeditionMacroLevel))
-			return "You can't build a caché here!";
-		if (!((OverworldExpeditionCell) performer.getLevel().getMapCell(performer.getPosition())).isLand())
-			return "You can't build a caché here!";
+			return "You can't drop your equipment here!";
 		return "";
 	}
 	
