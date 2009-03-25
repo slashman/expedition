1dc7e2fc13393040246ab867cf211bbfec759633
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2009-03-25 03:39:39 +0000

    Minor Balance Tweaks

--- src/net/slashie/expedition/ui/ExpeditionGenerator.java
+++ src/net/slashie/expedition/ui/ExpeditionGenerator.java
@@ -38,7 +38,7 @@ public abstract class ExpeditionGenerator {
 		ret.setName("Colombus");
 		ret.setExpeditionary("Colombus");
 		ret.setExpeditionaryTitle("Explorer");
-		ret.setAccountedGold(5000);
+		ret.setAccountedGold(2000);
 		List<Equipment> startingShips = new ArrayList<Equipment>();
 		startingShips.add(new Equipment(ItemFactory.createItem("CARRACK"), 3));
 		ret.setCurrentVehicles(startingShips);
@@ -51,7 +51,7 @@ public abstract class ExpeditionGenerator {
 		ret.addItemOffshore(sailor, 30);
 		ret.addItemOffshore(captain, 3);
 		ret.addItemOffshore(explorer, 1);
-		ret.addItemOffshore(ItemFactory.createItem("SPEARS"), 34);
+		ret.addItemOffshore(ItemFactory.createItem("SPEARS"), 15);
 		
 		for (int i = 0; i < 5000; i++){
 			ret.addItemOffshore(food, 100);
