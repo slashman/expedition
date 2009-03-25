196981be799cdeccb0084d262297c7158c2b96ca
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2009-03-25 03:39:14 +0000

    Used when dropping things to the sea

---
+++ src/net/slashie/expedition/domain/SeaPseudoCache.java
@@ -0,0 +1,14 @@
+package net.slashie.expedition.domain;
+
+import net.slashie.expedition.game.ExpeditionGame;
+
+public class SeaPseudoCache extends GoodsCache{
+	public SeaPseudoCache(ExpeditionGame game) {
+		super();
+	}
+	
+	public String getDescription() {
+		return "Sea";
+	}
+
+}
