65a960025c677a8bc8389eabfe2a187171227f91
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2009-03-28 17:48:09 +0000

    Check made harder to detect bugs, as stack trace was not being produced.

--- src/net/slashie/expedition/item/ItemFactory.java
+++ src/net/slashie/expedition/item/ItemFactory.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 import net.slashie.expedition.domain.ExpeditionItem;
 import net.slashie.expedition.domain.ExpeditionUnit;
 import net.slashie.expedition.game.ExpeditionGame;
+import net.slashie.utils.Debug;
 
 public class ItemFactory {
 	private static Hashtable<String, ExpeditionItem> definitions = new Hashtable<String, ExpeditionItem>();
@@ -16,8 +17,9 @@ public class ItemFactory {
 	public static ExpeditionItem createItem(String itemId){
 		ExpeditionItem ret = definitions.get(itemId);
 		if (ret == null){
-			ExpeditionGame.crash("Item "+itemId+" not found");
-			return null;
+			//ExpeditionGame.crash("Item "+itemId+" not found");
+			/*
+			return null;*/
 		}
 		return ret.clone();
 	}
