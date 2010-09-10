897253e5925a5906cb48facf5b03ed80098d0eea
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2010-09-10 02:57:45 +0000

    Fix http://slashie.net/mantis/view.php?id=1150

--- src/net/slashie/expedition/domain/SailingPoint.java
+++ src/net/slashie/expedition/domain/SailingPoint.java
@@ -3,14 +3,14 @@ package net.slashie.expedition.domain;
 import net.slashie.expedition.domain.Expedition.MovementSpeed;
 
 public enum SailingPoint {
-	BEATING ("Beating",0,44,MovementSpeed.SLOW),
-	CLOSE_REACH ("Close Reach",45,79,MovementSpeed.FAST),
-	BEAM_REACH ("Beam Reach", 80,109, MovementSpeed.NORMAL),
-	BROAD_REACH ("Broad Reach", 110, 159,MovementSpeed.SLOW),
-	RUNNING ("Running", 160, 180,MovementSpeed.SLOW),
+	BEATING ("Beating",0,44,MovementSpeed.VERY_SLOW),
+	CLOSE_REACH ("Close Reach",45,79,MovementSpeed.SLOW),
+	BEAM_REACH ("Beam Reach", 80,109, MovementSpeed.SLOW),
+	BROAD_REACH ("Broad Reach", 110, 159,MovementSpeed.NORMAL),
+	RUNNING ("Running", 160, 180,MovementSpeed.FAST),
 	
 	BARE_POLES ("Bare Poles",MovementSpeed.SLOW),
-	RUNNING_STORM ("Surfing",MovementSpeed.SLOW),
+	RUNNING_STORM ("Surfing",MovementSpeed.NORMAL),
 	
 	NONE ("None"),
 	STALLED ("Stalled");
