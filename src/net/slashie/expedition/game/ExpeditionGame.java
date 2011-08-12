0953034cb4bba3780bbfd2b7bdddac3d599abb98
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2011-08-12 04:29:02 +0000

    v0.3RC4

--- src/net/slashie/expedition/game/ExpeditionGame.java
+++ src/net/slashie/expedition/game/ExpeditionGame.java
@@ -171,7 +171,7 @@ public class ExpeditionGame extends SworeGame {
 	}
 	
 	public static String getVersion(){
-		return "v0.3 RC3";
+		return "v0.3 RC4";
 	}
 	
 	public Expedition getExpedition(){
