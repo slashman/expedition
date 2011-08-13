55b1b111bfa9c7820fb27ea47d2a7c15f42ef2ec
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2011-08-13 15:11:35 +0000

    Set framerate according to settings Remove synchronization for the drawing loop Alternate cheaper impl (Commented)

--- src/net/slashie/expedition/ui/oryx/sfx/RainEffect.java
+++ src/net/slashie/expedition/ui/oryx/sfx/RainEffect.java
@@ -60,6 +60,7 @@ public class RainEffect implements Runnable{
 	public void run() {
 		Rainlet[] rainlets = new Rainlet[maxRainlets];
 		int usedRainlets = 0;
+		int sleep = (int)Math.round((double)si.getFrameRate() * (5.0d/4.0d));
 		while (true){
 			if (!commandsQueue.isEmpty()){
 				String command = null;
@@ -93,8 +94,91 @@ public class RainEffect implements Runnable{
 				}
 			}
 			
+
+			si.cleanLayer(ExpeditionOryxUI.SFX_LAYER);
+			Graphics2D g = si.getDrawingGraphics(ExpeditionOryxUI.SFX_LAYER);
+			g.setColor(rainColor);
+			// Draw rainlets				
+			for (Rainlet rainlet: rainlets){
+				for (int i = 0; i < rainlet.size; i++){
+					g.fillRect(-rainlet.fall*(rainlet.speed*2)+rainlet.position.x + i*2, rainlet.fall*(rainlet.speed*2)+rainlet.position.y-i*2, 2, 2);
+				}
+			}
+			si.commitLayer(ExpeditionOryxUI.SFX_LAYER);
 			
-			synchronized (si) {
+			try {
+				Thread.sleep(sleep);
+			} catch (InterruptedException e) {}
+		}
+	}
+	
+	/*
+	 * public void run() {
+		Rainlet[] rainlets = new Rainlet[maxRainlets];
+		
+		// Generate a String of randomness
+		int[] random0to800 = new int[50];
+		int[] random0to600 = new int[50]; 
+		int[] randomSizes = new int[50];
+		int[] randomSpeeds = new int[50];
+		int[] randomSalts = new int[80];
+		
+		for (int i = 0; i < 50; i++){
+			random0to800[i] = Util.rand(0, 800);
+			random0to600[i] = Util.rand(0, 600);
+			randomSizes[i] = Util.rand(minSize, maxSize);
+			randomSpeeds[i] = Util.rand(rainSpeed, rainSpeed+2);
+		}
+		for (int i = 0; i < 80; i++){
+			randomSalts[i] = Util.rand(0, 15);
+		}
+
+		int randomIndex = 0;
+		int saltIndex = 0;
+		while (true){
+			if (!commandsQueue.isEmpty()){
+				String command = null;
+				while (command == null){
+					try {
+						command = commandsQueue.take();
+					} catch (InterruptedException e) {}
+				}
+				if (command.equals("KILL")){
+					si.cleanLayer(ExpeditionOryxUI.SFX_LAYER);
+					si.commitLayer(ExpeditionOryxUI.SFX_LAYER);
+
+					break;
+				}
+			}
+			
+			// Create new rainlets
+			
+			for (int i = 0; i < maxRainlets; i++){
+				if (rainlets[i] == null){
+					rainlets[i] = new Rainlet(new Position(Util.rand(0, 800), Util.rand(0, 600)), Util.rand(minSize, maxSize), Util.rand(rainSpeed, rainSpeed+2)); 
+				} else if (rainlets[i].dead) {
+					int index = (randomSalts[saltIndex] + randomIndex) % 50;
+					rainlets[i].position.x = random0to800[randomIndex];
+					rainlets[i].position.y = random0to600[index];
+					rainlets[i].size = randomSizes[index];
+					rainlets[i].speed = randomSpeeds[index];
+					rainlets[i].fall = 0;
+					rainlets[i].dead = false;
+					randomIndex++;
+					if (randomIndex >= 50){
+						randomIndex = 0;
+					}
+					saltIndex++;
+					if (saltIndex >= 80){
+						saltIndex = 0;
+					}
+				} else {
+					rainlets[i].evolve();
+				}
+			}
+			
+			
+			//synchronized (si) {
 				si.cleanLayer(ExpeditionOryxUI.SFX_LAYER);
 				Graphics2D g = si.getDrawingGraphics(ExpeditionOryxUI.SFX_LAYER);
 				g.setColor(rainColor);
@@ -106,10 +190,11 @@ public class RainEffect implements Runnable{
 				}
 				
 				si.commitLayer(ExpeditionOryxUI.SFX_LAYER);
-			}
+			//}
 			try {
-				Thread.sleep(10);
+				Thread.sleep(si.getFrameRate());
 			} catch (InterruptedException e) {}
 		}
-	}
+	}*/
+	 
 }
