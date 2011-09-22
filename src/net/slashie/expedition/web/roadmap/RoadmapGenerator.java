f26476b8db4cc5cecc030754d1ee070a418fd828
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2011-09-22 18:09:49 +0000

    Improvements over the roadmap

--- src/net/slashie/expedition/web/roadmap/RoadmapGenerator.java
+++ src/net/slashie/expedition/web/roadmap/RoadmapGenerator.java
@@ -25,7 +25,9 @@ public class RoadmapGenerator {
 	static class Facet {
 		String id;
 		String name;
+		String projectedRelease;
 		List<FunctionalComponent> functionalComponents;
+		List<FundingCampaign> fundingCampaigns;
 		int implementationOrder;
 		public int getTotalComplexityPoints() {
 			int ret = 0;
@@ -42,10 +44,17 @@ public class RoadmapGenerator {
 		int complexity;
 	}
 	
+	static class FundingCampaign {
+		String name;
+		String url;
+		String deadline;
+		String goal;
+	}
+	
 	public static void main(String... args){
 		try {
 			List<Facet> facets = parseXML("roadmap.xml");
-			BufferedWriter br = new BufferedWriter(new FileWriter("roadmap.html"));
+			BufferedWriter br = new BufferedWriter(new FileWriter("index.html"));
 			writeHeader(br);
 			writeFacets(br,facets);
 			writeFooter(br);
@@ -90,16 +99,18 @@ public class RoadmapGenerator {
 					Facet f = new Facet();
 					f.id = baseElement.getAttribute("id");
 					f.name = baseElement.getAttribute("name");
+					f.projectedRelease = baseElement.getAttribute("projectedRelease");
 					f.functionalComponents = new ArrayList<FunctionalComponent>();
+					f.fundingCampaigns = new ArrayList<FundingCampaign>();
 					f.implementationOrder = Integer.parseInt(baseElement.getAttribute("implementationOrder"));
-					processFunctionalComponents(baseElement, f.functionalComponents);
+					processFacet(baseElement, f);
 					facets.add(f);
 				}
 			}
 		}
 	}
 	
-	private static void processFunctionalComponents(Element facetElement, List<FunctionalComponent> functionalComponents) {
+	private static void processFacet(Element facetElement, Facet facet) {
 		NodeList baseNodes = facetElement.getChildNodes();
 		for (int i = 0; i < baseNodes.getLength(); i++){
 			Node baseNode = baseNodes.item(i);
@@ -110,8 +121,16 @@ public class RoadmapGenerator {
 					f.name = baseElement.getAttribute("name");
 					f.description = baseElement.getAttribute("description");
 					f.complexity = Integer.parseInt(baseElement.getAttribute("complexity"));
-					functionalComponents.add(f);
+					facet.functionalComponents.add(f);
+				} else if (baseElement.getTagName().equals("fundingCampaign")){
+					FundingCampaign f = new FundingCampaign();
+					f.name = baseElement.getAttribute("name");
+					f.deadline = baseElement.getAttribute("deadline");
+					f.url = baseElement.getAttribute("url");
+					f.goal = baseElement.getAttribute("goal");
+					facet.fundingCampaigns.add(f);
 				}
+
 			}
 		}
 	}
@@ -129,16 +148,19 @@ public class RoadmapGenerator {
 		}
 	}
 	
-	
 	private static void writeFacet(BufferedWriter br, Facet facet) throws IOException {
 		// Open Casing
 		br.write("\n<table class = \"facetTable\">");
-		br.write("<tr><td class = \"nwCell\"/><td class = \"nCell\"/><td class = \"neCell\"/></tr>");
-		br.write("<tr><td class = \"wCell\"/><td class = \"cCell\">");
+		br.write("<tr><td class = \"nwCell\"></td><td class = \"nCell\"></td><td class = \"neCell\"></td></tr>");
+		br.write("<tr><td class = \"wCell\"></td><td class = \"cCell\">");
 		
 		// Inner table
-		br.write("<table class = \"innerTable\"><tr><td colspan = \"2\"><b>"+facet.name+" Facet</b> ("+facet.getTotalComplexityPoints()+" complexity points, Ranked #"+facet.implementationOrder+")</td></tr>");
-		br.write("<tr><td><img src = \"img/"+facet.id+".png\"/></td><td>");
+		br.write("<table class = \"innerTable\"><tr><td colspan = \"2\"><b>"+facet.name+" Facet</b> ("+facet.getTotalComplexityPoints()+" complexity points, Implementation Order #"+facet.implementationOrder+")");
+		if (null != facet.projectedRelease && !facet.projectedRelease.equals("")){
+			br.write(", Projected release date: "+facet.projectedRelease);
+		}
+		br.write("</td></tr>");
+		br.write("<tr><td><img src = \"img/"+facet.id+".png\" alt = \""+facet.name+"\"></td><td>");
 		br.write("<ul>");
 		for (FunctionalComponent functionalComponent: facet.functionalComponents){
 			br.write("<li><b>"+functionalComponent.name+"</b>: "+functionalComponent.description+". <b>"+functionalComponent.complexity+" complexity points</b>");
@@ -147,9 +169,19 @@ public class RoadmapGenerator {
 		br.write("</td>");
 		br.write("</table>");
 		
+		if (facet.fundingCampaigns.size() > 0){
+			br.write("<b>Current funding campaigns:</b><br><ul>");
+			for (FundingCampaign fundingCampaign: facet.fundingCampaigns){
+				br.write("<li><a href = \""+fundingCampaign.url+"\">"+fundingCampaign.name+"</a>. Goal: "+fundingCampaign.goal+", deadline: "+fundingCampaign.deadline+"</li>");
+			}
+			br.write("</ul>");
+		} else {
+			br.write("<p>No active campaigns for this facet, if you want to fund its development please:<ul><li><a href = \"http://slashware.net/blog/contribute\">Become a supporter</a> and contact us, we will have your contribution in mind when scheduling the facets implementation</li><li>Fund one of our other facets, we will eventually get to this one :)</li></ul></p>");
+		}
+		
 		// Close casing
-		br.write("<td class = \"eCell\"/></tr>");
-		br.write("<tr><td class = \"swCell\"/><td class = \"sCell\"/><td class = \"seCell\"/></tr>");
+		br.write("<td class = \"eCell\"></td></tr>");
+		br.write("<tr><td class = \"swCell\"></td><td class = \"sCell\"></td><td class = \"seCell\"></td></tr>");
 		br.write("</table>");
 	}
 
