9ac10bef32faa2f7172f86a3c98e60e73ec057b9
java.koder <java.koder@9f61a58a-1354-11de-9433-ad2e3fad96c5>
2011-11-12 06:16:51 +0000

    0.5 alpha 1

--- src/net/slashie/expedition/game/ExpeditionGame.java
+++ src/net/slashie/expedition/game/ExpeditionGame.java
@@ -61,7 +61,7 @@ public class ExpeditionGame extends SworeGame {
 		
 	}
 
-	private static final ExpeditionVersion THIS_VERSION = new ExpeditionVersion ("0.4.2", 2011, 11, 12);
+	private static final ExpeditionVersion THIS_VERSION = new ExpeditionVersion ("0.5 alpha 1", 2011, 11, 12);
 	
 	private static ExpeditionGame currentGame;
 	private int lastExpeditionId = 1;
